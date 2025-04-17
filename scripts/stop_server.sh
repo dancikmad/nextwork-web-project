42ac2714b54630a8a3867300c539f1187f0d08a0
Daniel Crigan <crgdaniel@yahoo.com>
2025-04-17 21:02:49 +0000

    Adding CodeDeploy files

---
+++ scripts/stop_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+isExistApp="$(grep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi 
+
+isExistApp="$(grep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
+
