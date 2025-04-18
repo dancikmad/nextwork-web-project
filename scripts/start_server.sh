f194e5a4ce58b403ff9892d4768ec5c824f4f32d
Daniel Crigan <crgdaniel@yahoo.com>
2025-04-18 01:24:24 +0000

    fix: typo in httpd service start

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl start tomcat.service
 sudo systemctl enable tomcat.service
-sudo systemctl start http
-sudo systemctl enable httpd
+sudo systemctl start http.service
+sudo systemctl enable httpd.service
 
 
