3b02259b7416cf512ec78ffd2e94ad4a92580e96
Daniel Crigan <crgdaniel@yahoo.com>
2025-04-18 15:50:08 +0100

    Fix typo in stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Adding this line to intentionally break deployment 
-# and practice emergency recovery to restore service
-set -e 
-
-# Intentional error
-sudo systemcll stop httpd.service # Misspelled command will now cause script to exit with non zero-status
-
-# To be extra sure, we can also add an explicit exit code
-exit 1
-
+isExistApp="$(grep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp="$(grep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
 
